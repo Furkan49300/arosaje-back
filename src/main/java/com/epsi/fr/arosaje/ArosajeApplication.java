dd35ff655d6aa1daf4d54c365c38c5a9a54b4a69
furkan <zarodza49@gmail.com>
2024-05-29 11:57:21 +0200

    test

--- src/main/java/com/epsi/fr/arosaje/ArosajeApplication.java
+++ src/main/java/com/epsi/fr/arosaje/ArosajeApplication.java
@@ -7,16 +7,29 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 
 @SpringBootApplication
+@CrossOrigin("*")
 public class ArosajeApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ArosajeApplication.class, args);
 	}
-
+@RestController
+	@RequestMapping("/")
+	static class HomeController {
+		@GetMapping
+	String showTimeStamp() {
+			return LocalDateTime.now().toString();
+		}
+}
 }
