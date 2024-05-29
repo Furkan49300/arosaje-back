4e9f861748e5140f3822283a8872ede8f56ffc35
furkan <zarodza49@gmail.com>
2024-05-29 16:02:38 +0200

    cor

--- src/main/java/com/epsi/fr/arosaje/WebConfig.java
+++ src/main/java/com/epsi/fr/arosaje/WebConfig.java
@@ -1,4 +1,5 @@
 package com.epsi.fr.arosaje;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
@@ -13,7 +14,8 @@ public class WebConfig {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.addAllowedOriginPattern("https://arosaje-crud.*"); // Use pattern for allowed origins
+        config.addAllowedOrigin("https://arosaje-crud-lhp5lpcvh-furkan49300s-projects.vercel.app"); // Add specific origin
+        // config.addAllowedOriginPattern("https://arosaje-crud.*"); // Comment out the pattern for testing
         config.addAllowedHeader("*");
         config.addAllowedMethod("OPTIONS");
         config.addAllowedMethod("GET");
