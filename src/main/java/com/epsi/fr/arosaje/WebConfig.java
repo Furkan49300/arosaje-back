8c593ef89f8fc18a928e132ee414c16c8f78d955
furkan <zarodza49@gmail.com>
2024-05-29 15:41:08 +0200

    corss

---
+++ src/main/java/com/epsi/fr/arosaje/WebConfig.java
@@ -0,0 +1,18 @@
+package com.epsi.fr.arosaje;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("https://arosaje-crud-mgz9swnem-furkan49300s-projects.vercel.app")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
